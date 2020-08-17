005592c595da62b6a9fe2cabd0de176dcf7521cb
Hendoe <hendo1349@gmail.com>
2020-08-17 15:41:40 -0500

    api works

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const apiKey = "833ea1b9d7mshbef97797dff363dp1d9ac4jsna2801a24e32d";
+const apiKey = 'ZNYoYacMbpemidFbebDE7svfp2rIHbU7qqGpMGRg';
 
-const searchURL = 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/NewsSearchAPI';
-
+const searchURL = 'https://developer.nps.gov/api/v1/alerts?parkCode=acad,dena';
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
@@ -11,52 +10,69 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
-function displayResults(responseJson, maxResults) {
-  // if there are previous results, remove them
-  console.log(responseJson);
-  $('#results-list').empty();
-  // iterate through the articles array, stopping at the max number of results
-  for (let i = 0; i < responseJson.value.length & i<maxResults ; i++){
-    // for each video object in the articles
-    //array, add a list item to the results 
-    //list with the article title, source, author,
-    //description, and image
-    $('#results-list').append(
-      `<li><h3><a href="${responseJson.value[i].url}">${responseJson.value[i].title}</a></h3>
-      <p>${responseJson.value[i].description}</p>
-      <p>By ${responseJson.value[i].body}</p>
-      </li>`
-    )};
-  //display the results section  
-  $('#results').removeClass('hidden');
-};
+// function displayResults(responseJson, maxResults) {
+//   // if there are previous results, remove them
+//   console.log(responseJson);
+//   $('#results-list').empty();
+//   // iterate through the articles array, stopping at the max number of results
+//   for (let i = 0; i < responseJson.length & i<maxResults ; i++){
+//     // for each video object in the articles
+//     //array, add a list item to the results 
+//     //list with the article title, source, author,
+//     //description, and image
+//     $('#results-list').append(
+//       `<li><h3><a href="${responseJson[i].url}">${responseJson[i].title}</a></h3>
+//       <p>${responseJson[i].description}</p>
+//       <p>By ${responseJson[i].body}</p>
+//       </li>`
+//     )};
+//   //display the results section  
+//   $('#results').removeClass('hidden');
+// };
 
-function getNews(query, maxResults=10) {
+function displayResults(response, maxResults) {
+    console.log(responseJson);
+}
+
+function getParks(query, maxResults=10) {
   const params = {
+    // parkCode: [],
+    // stateCode: [],
+    // limit: '',
+    // start: '',
     q: query,
-    pageSize: maxResults
+    // sort: [],
+    // pageSize: maxResults
   };
   const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
+  //const url = searchURL + '?api_key=' + apiKey + '&' + queryString;
+  //const url = `https://developer.nps.gov/api/v1/alerts?parkCode=acad,dena&api_key=ZNYoYacMbpemidFbebDE7svfp2rIHbU7qqGpMGRg`
+  const url = `https://developer.nps.gov/api/v1/parks?q=moutain&api_key=ZNYoYacMbpemidFbebDE7svfp2rIHbU7qqGpMGRg`
 
   console.log(url);
 
-  const options = {
-    headers: new Headers({
-      "x-rapidapi-key": apiKey})
-  };
+//   const options = {
+//     headers: new Headers({
+//       "x-rapidapi-key": apiKey})
+//   };
 
-  fetch(url, options)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson, maxResults))
-    .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });
+        fetch(url)
+            .then(response => response.json())
+            .then(responseJson => {
+                console.log(responseJson);
+                //renderDogs(responseJson);
+            });
+//   fetch(url, options)
+//     .then(response => {
+//       if (response.ok) {
+//         return response.json();
+//       }
+//       throw new Error(response.statusText);
+//     })
+//     .then(responseJson => displayResults(responseJson, maxResults))
+//     .catch(err => {
+//       $('#js-error-message').text(`Something went wrong: ${err.message}`);
+//     });
 }
 
 function watchForm() {
@@ -64,7 +80,7 @@ function watchForm() {
     event.preventDefault();
     const searchTerm = $('#js-search-term').val();
     const maxResults = $('#js-max-results').val();
-    getNews(searchTerm, maxResults);
+    getParks(searchTerm, maxResults);
   });
 }
 
